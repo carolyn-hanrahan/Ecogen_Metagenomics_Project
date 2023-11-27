d5bd126419c30beefe29f139e89771bde2c0909e
carolyn-hanrahan <122393990+carolyn-hanrahan@users.noreply.github.com>
2023-11-27 10:59:24 -0500

    update script with imported data

---
+++ Script_Nov_27_2023.R
@@ -0,0 +1,60 @@
+# Notes from November 27, 2023
+# Carolyn Hanrahan 
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## Set your working directory
+setwd("C:/Users/Carolyn/Desktop/Ecogen Group Project/Ecogen_Metagenomics_Project/ITS_data") 
+
+
+#install.packages("readxl")
+library(readxl)
+library(dplyr)
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("phyloseq") # Install phyloseq
+
+
+install.packages(c("RColorBrewer", "patchwork")) #install patchwork to chart publication-quality plots and readr to read rectangular datasets.
+
+library("phyloseq")
+library("ggplot2")
+library("RColorBrewer")
+library("patchwork")
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Specify the file path for ITS data 
+ITS_file_path <- "C:/Users/Carolyn/Desktop/Ecogen Group Project/Ecogen_Metagenomics_Project/ITS_data/ITS_Sequence_Data_clean.xlsx"
+
+# Read the Excel file & Import ITS data
+its_data <- read_excel(ITS_file_path)
+
+head(its_data)
+
+
+#Specify the file path for 16S data
+sixteen_s_file_path <- "C:/Users/Carolyn/Desktop/Ecogen Group Project/Ecogen_Metagenomics_Project/16S_data/16S_Sequence_Data_clean.xlsx"
+
+# Read the Excel file and import 16S data: 
+sixteen_s_data <- read_excel(sixteen_s_file_path)
+
+head(sixteen_s_data)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Practice Code
+
+# converting excel file into a dataframe: 
+
+its_df <- as.data.frame(its_data)
+head(its_df)
+
+# subsetting dataframe to examine sample numbers and corresponding treatments:
+samples_and_treatment <- its_df[ ,1:2]
+
+#
+
+merged_metagenomes <- import_biom(its_df)
+
+
+
